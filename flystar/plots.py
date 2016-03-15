eac99153e837daaa2f9b6d4ca1449b2460ae29d2
Matt <mwhosek@gmail.com>
2016-03-14 16:42:05 -1000

    Added other diagnostic plots

--- flystar/plots.py
+++ flystar/plots.py
@@ -19,7 +19,7 @@ def trans_positions(ref, ref_mat, starlist, starlist_mat):
 
     ref_mat: astropy table
         Reference starlist only containing matched stars that were used in the
-        transformation
+        transformation. Standard column headers are assumed.
 
     starlist: astropy table
         Transformed starist with the reference starlist coordinates.
@@ -46,14 +46,26 @@ def trans_positions(ref, ref_mat, starlist, starlist_mat):
     return
 
 
+def posDiff_hist(ref_mat, starlist_mat):
+    """
+    Plot histogram of position differences for the matched
+    stars: reference - starlist
 
+    Parameters:
+    -----------
+    ref_mat: astropy table
+        Reference starlist only containing matched stars that were used in the
+        transformation. Standard column headers are assumed.
+        
+    starlist_mat: astropy table
+        Transformed starlist only containing the matched stars used in
+        the transformation. Standard column headers are assumed. 
+    
+    """
+    diff_x = ref_mat['x'] - starlist_mat['x']
+    diff_y = ref_mat['y'] - starlist_mat['y']
 
-    # Histogram of position differences for the matched stars:
-    # reference - label.dat
-    diff_x = starlist_mat['x'] - label_mat['x']
-    diff_y = starlist_mat['y'] - label_mat['y']
-
-    py.figure(2, figsize=(10,10))
+    py.figure(figsize=(10,10))
     py.clf()
     py.hist(diff_x, bins=25, color='blue', label='X')
     py.hist(diff_y, bins=25, color='red', label='Y')
@@ -63,24 +75,66 @@ def trans_positions(ref, ref_mat, starlist, starlist_mat):
     py.legend()
     py.savefig('Positions_hist.png')
 
-    # Looking at quiverplot of differences relative to position in image.
-    # These are all the stars that were used in the transformation
-    py.figure(3, figsize=(10,10))
-    py.clf()
-    q = py.quiver(starlist_mat['x'], starlist_mat['y'], diff_x, diff_y, scale=10)
-    py.quiverkey(q, 0.2, 0.92, 0.2, '0.2 pix', coordinates='figure', color='black')
-    py.xlabel('X Position (Reference, pix)')
-    py.ylabel('Y Position (Reference, pix)')
-    py.title('Reference - Transformed label.dat positions')
-    py.savefig('Positions_quiver.png')
-    
-    # Look at the histogram of mag diff for matched stars, as well
-    diff_m = starlist_mat['m'] - label_mat['m']
-    
-    py.figure(4, figsize=(10,10))
+    return
+
+def magDiff_hist(ref_mat, starlist_mat):
+    """
+    Plot histogram of mag differences for the matched
+    stars: reference - starlist
+
+    Parameters:
+    -----------
+    ref_mat: astropy table
+        Reference starlist only containing matched stars that were used in the
+        transformation. Standard column headers are assumed.
+        
+    starlist_mat: astropy table
+        Transformed starlist only containing the matched stars used in
+        the transformation. Standard column headers are assumed.         
+
+    """
+    diff_m = ref_mat['m'] - starlist_mat['m']
+
+    py.figure(figsize=(10,10))
     py.clf()
     py.hist(diff_m, bins=25)
     py.xlabel('Reference Mag - label.dat Mag')
     py.ylabel('N stars')
     py.title('Magnitude Match')
     py.savefig('Magnitude_hist.png')
+
+    return
+
+def posDiff_quiver(ref_mat, starlist_mat, qscale=10):
+    """
+    Plot histogram of position differences for the matched
+    stars: reference - starlist
+
+    Parameters:
+    -----------
+    ref_mat: astropy table
+        Reference starlist only containing matched stars that were used in the
+        transformation. Standard column headers are assumed.
+        
+    starlist_mat: astropy table
+        Transformed starlist only containing the matched stars used in
+        the transformation. Standard column headers are assumed.
+
+    qscale: int (default=10)
+        Scale parameter for the quiver plot
+    
+    """
+    diff_x = ref_mat['x'] - starlist_mat['x']
+    diff_y = ref_mat['y'] - starlist_mat['y']
+
+    
+    py.figure(figsize=(10,10))
+    py.clf()
+    q = py.quiver(ref_mat['x'], ref_mat['y'], diff_x, diff_y, scale=10)
+    py.quiverkey(q, 0.2, 0.92, 0.2, '0.2 pix', coordinates='figure', color='black')
+    py.xlabel('X Position (Reference, pix)')
+    py.ylabel('Y Position (Reference, pix)')
+    py.title('Reference - Transformed label.dat positions')
+    py.savefig('Positions_quiver.png')
+
+    return
