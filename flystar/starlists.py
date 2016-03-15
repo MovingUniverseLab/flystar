0578a9c19d355b71ce1125af1e7f54e319edc7b0
Matt <mwhosek@gmail.com>
2016-03-14 15:50:22 -1000

    Added function to restrict by area

--- flystar/starlists.py
+++ flystar/starlists.py
@@ -28,3 +28,39 @@ def restrict_by_name(table1, table2):
     idx2 = np.array([list(name2).index(i) for i in Name])
 
     return table1[idx1], table2[idx2], len(idx1)
+
+def restrict_by_area(table1, area):
+    """
+    Restrict starlist to those within a specific area. Note: the output starlist
+    will ONLY contain stars that fulfills the area condition. 
+
+    Parameters:
+    ----------
+    table1: astropy table
+         Starlist to be restricted. Must have standard column headers
+         (i.e. x, y)
+
+    area: 2x2 array [[x1, x2], [y1, y2]]
+        X and Y coordinate range to restric the stars too. Only stars with
+        coordinates with x1 < X < x2 and y1 < Y < y2 will be allowed.
+    
+    Output:
+    ------
+    astropy table: same as input table, only with stars that pass the area
+    restriction
+    """
+    # Extract star coordinates
+    xpos = table1['x']
+    ypos = table1['y']
+    
+    # Extract desired coordinate ranges
+    x_range = area[0]
+    y_range = area[1]
+
+    # Apply restriction
+    good = np.where( (xpos > x_range[0]) & (xpos < x_range[1]) &
+                      (ypos > y_range[0]) & (ypos < y_range[1]) )
+
+    table_out = table1[good]
+
+    return table_out
