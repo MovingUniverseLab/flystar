d53bf19d16700d08dc5825367755c43d101db7b7
Jessica Lu <jlu.astro@yahoo.com>
2017-10-03 12:48:03 -0700

    Fixed star table to require numpy arrays, etc.

--- flystar/startables.py
+++ flystar/startables.py
@@ -1,88 +1,131 @@
 from astropy.table import Table, Column
+import numpy as np
+import pdb
 
 
 class StarTable(Table):
     """
-    A startable is an astropy.Table with stars matched from multiple starlists. 
-    I will describe the arguments. 
-    """
+    A StarTable is an astropy.Table with stars matched from multiple starlists. 
+
+    Required table columns (input as keywords):
+    -------------------------
+    name : 1D numpy.array with shape = N_stars
+        List of unique names for each of the stars in the table.
+
+    x : 2D numpy.array with shape = (N_stars, N_lists)
+        Positions of N_stars in each of N_lists in the x dimension.
+
+    y : 2D numpy.array with shape = (N_stars, N_lists)
+        Positions of N_stars in each of N_lists in the y dimension.
+
+    m : 2D numpy.array with shape = (N_stars, N_lists)
+        Magnitudes of N_stars in each of N_lists.
     
-    def __init__(self, ref=0, time_lists=list(), name_lists=list(), **kwargs):
+    Optional table columns (input as keywords):
+    -------------------------
+    xe : 2D numpy.array with shape = (N_stars, N_lists)
+        Position uncertainties of N_stars in each of N_lists in the x dimension.
+
+    ye : 2D numpy.array with shape = (N_stars, N_lists)
+        Position uncertainties of N_stars in each of N_lists in the y dimension.
+
+    me : 2D numpy.array with shape = (N_stars, N_lists)
+        Magnitude uncertainties of N_stars in each of N_lists.
+
+    ep_name : 2D numpy.array with shape = (N_stars, N_lists)
+        Names in each epoch for each of N_stars in each of N_lists. This is 
+        useful for tracking purposes. 
+
+    Optional table meta data
+    -------------------------
+    list_names : list of strings
+        List of names, one for each of the starlists.
+
+    list_times : list of floats
+        List of times/dates for each starlist.
+
+    ref_list : int
+        Specify which list is the reference list (if any). 
+    
+    """
+    def __init__(self, ref_list=0, **kwargs):
         """
         """
         
         # Check if the required arguments are present
-        arg_req = ('x', 'y', 'm')
+        arg_req = ('name', 'x', 'y', 'm')
         
         for arg_test in arg_req:
             if arg_test not in kwargs:
-                raise TypeError("The StarTable class requires a '%s' argument"
-                                % arg_test)
+                err_msg = "The StarTable class requires a '{0:s}' argument"
+                raise TypeError(err_msg.format(arg_test))
 
-        # TO DO: I thought we didn't require these? 
+        # If we have errors, we need them in both dimensions.
         if ('xe' in kwargs) ^ ('ye' in kwargs):
             raise TypeError("The StarTable class requires both 'xe' and" +
                             " 'ye' arguments")
+
+        # Figure out the shape 
+        n_stars = kwargs['x'].shape[0]
+        n_lists = kwargs['x'].shape[1]
         
-        # Check if the type and size of the arguments are correct
-        arg_tab = ('x', 'y', 'm', 'xe', 'ye', 'me', 'name')
-        n_stars = len(kwargs['x'])
-        n_lists = len(kwargs['x'].colnames)
+        # Check if the type and size of the arguments are correct.
+        # Name checking: type and shape
+        if (not isinstance(kwargs['name'], np.ndarray)) or (len(kwargs['name']) != n_stars):
+            err_msg = "The '{0:s}' argument has to be a numpy array "
+            err_msg += "with length = {1:d}"
+            raise TypeError(err_msg.format('name', n_stars))
+
+        # Check all the 2D arrays.
+        arg_tab = ('x', 'y', 'm', 'xe', 'ye', 'me', 'ep_name')
         
         for arg_test in arg_tab:
-            
             if arg_test in kwargs:
-                # TO DO: Why does this have to be a Table? Don't we want a 2D array or a
-                # Column object? A table has lots of other stuff too... so I don't think
-                # that would work.
-                if not isinstance(kwargs[arg_test], Table):
-                    raise TypeError("The '%s' table has to be an astropy table"
-                                    % arg_test)
+                if not isinstance(kwargs[arg_test], np.ndarray):
+                    err_msg = "The '{0:s}' argument has to be a numpy array"
+                    raise TypeError(err_msg.format(arg_test))
             
-                if len(kwargs[arg_test]) != n_stars:
-                    raise TypeError("The '%s' table has to have " +
-                                    str(n_stars) + " stars" % arg_test)
-            
-                if len(kwargs[arg_test].colnames) != n_lists:
-                    raise TypeError("The '%s' table has to have " +
-                                    str(n_lists) + " lists" % arg_test)
-        
-        if ref not in range(n_lists):
-            raise IndexError("The 'ref' argument has to be an integer" +
-                             "between 0 and " + str(n_lists - 1))
+                if kwargs[arg_test].shape != (n_stars, n_lists):
+                    err_msg = "The '{0:s}' argument has to have shape = ({1:d}, {2:d})"
+                    raise TypeError(err_msg.format(arg_test, n_stars, n_lists))
 
-        if not all(isinstance(tt, (int, float)) for tt in time_lists):
-            raise TypeError("The 'time_lists' argument has to be a list of" +
-                            " numbers")
-        
-        if not all(isinstance(nn, str) for nn in name_lists):
-            raise TypeError("The 'name_lists' argument has to be a list of" +
-                            " strings")
-        
-        if (len(time_lists) != 0) & (len(time_lists) != n_lists):
-            raise ValueError("The 'time_lists' argument has to have " +
-                             str(n_lists) + " lists")
-        
-        if (len(name_lists) != 0) & (len(name_lists) != n_lists):
-            raise ValueError("The 'name_lists' argument has to have " +
-                             str(n_lists) + " lists")
-        
+        # Check that the reference list is specified. 
+        if ref_list not in range(n_lists):
+            err_msg = "The 'ref_list' argument has to be an integer between 0 and {0:d}"
+            raise IndexError(err_msg.format(n_lists-1))
+
+        # We have to have special handling of meta-data (i.e. info that has
+        # dimensions of n_lists).
+        meta_tab = ('list_times', 'list_names')
+        meta_type = (float, str)
+        for mm in range(len(meta_tab)):
+            meta_test = meta_tab[mm]
+            meta_type_test = meta_type[mm]
+            
+            if meta_test in kwargs:
+                if len(kwargs[meta_test]) != n_lists:
+                    err_msg = "The '{0:s}' argument has to have length = {1:d}"
+                    raise ValueError(err_msg.format(meta_test, n_lists))
+            
+                if not all(isinstance(tt, meta_type_test) for tt in kwargs[meta_test]):
+                    err_msg = "The '{0:s}' argument has to be a list of {1:s}."
+                    raise TypeError(err_msg.format(meta_test, str(meta_type_test)))
+
+        #####
         # Create the startable
-        Table.__init__(self, (kwargs['x'], kwargs['y'], kwargs['m']),
-                       names=('x', 'y', 'm'))
-        self.meta = {'n_stars': n_stars, 'n_lists': n_lists, 'ref': ref,
-                     'time_lists': time_lists, 'name_lists': name_lists}
-        
-        if ('xe' in kwargs) & ('ye' in kwargs):
-            self.add_columns([Column(data=kwargs['xe'], name='xe'),
-                              Column(data=kwargs['ye'], name='ye')])
-        
-        if 'me' in kwargs:
-            self.add_column(Column(data=kwargs['me'], name='me'))
-        
-        if 'name' in kwargs:
-            self.add_column(Column(data=kwargs['name'], name='name'))
+        #####
+        Table.__init__(self, (kwargs['name'], kwargs['x'], kwargs['y'], kwargs['m']),
+                       names=('name', 'x', 'y', 'm'))
+        self.meta = {'n_stars': n_stars, 'n_lists': n_lists, 'ref_list': ref_list}
 
-        # Assign a random new name.
-        uname = ["star_{:06d}".format(item) for item in range(1, (n_stars + 1))]
-        self.add_column(Column(data=uname, name='uname'))
+        for meta_arg in meta_tab:
+            if meta_arg in kwargs:
+                self.meta[meta_arg] = kwargs[meta_arg]
+
+        for arg in arg_tab:
+            if arg in ['name', 'x', 'y', 'm']:
+                continue
+            if arg in kwargs:
+                self.add_column(Column(data=kwargs[arg], name=arg))
+
+        return
--- flystar/tests/test_startable.py
+++ flystar/tests/test_startable.py
@@ -1,41 +1,56 @@
 from astropy.table import Table
 from flystar.startables import StarTable
+import numpy as np
+import os
 
+test_dir = os.path.dirname(__file__)
 
 def test_StarTable_init1():
     """
     Test creation of new StarTable.
     """
     # User input
-    cat_file = 'test_catalog.fits'
+    cat_file = test_dir + '/test_catalog.fits'
 
     # Read and arrange the test input
     cat_tab = Table.read(cat_file)
-    
-    name_tab = Table([cat_tab['name'], cat_tab['name'], cat_tab['name']],
-                    names=('1', '2', '3'))
-    x_tab = Table([cat_tab['x'][:, 1], cat_tab['x'][:, 2], cat_tab['x'][:, 3]],
-                      names=('1', '2', '3'))
-    y_tab = Table([cat_tab['y'][:, 1], cat_tab['y'][:, 2], cat_tab['y'][:, 3]],
-                names=('1', '2', '3'))
-    m_tab = Table([cat_tab['m'][:, 1], cat_tab['m'][:, 2], cat_tab['m'][:, 3]],
-                names=('1', '2', '3'))
-    xe_tab = Table([cat_tab['xe'][:, 1], cat_tab['xe'][:, 2], cat_tab['xe'][:, 3]],
-                names=('1', '2', '3'))
-    ye_tab = Table([cat_tab['ye'][:, 1], cat_tab['ye'][:, 2], cat_tab['ye'][:, 3]],
-                names=('1', '2', '3'))
-    me_tab = Table([cat_tab['me'][:, 1], cat_tab['me'][:, 2], cat_tab['me'][:, 3]],
-                names=('1', '2', '3'))
-    time_lists = [2001, 2002.1, 2003.0]
-    name_lists = ['file1', 'file2', 'file3']
+
+    N_stars = len(cat_tab)
+    N_lists = cat_tab['x'].shape[1]
+    print(N_stars, N_lists)
+    print(cat_tab['x'].shape)
+
+    # Make a fake 2D array of names per epoch. We will call them "id".
+    # Note that all of these inputs will be numpy arrays.
+    x_in = cat_tab['x'].data
+    y_in = cat_tab['y'].data
+    m_in = cat_tab['m'].data
+    xe_in = cat_tab['xe'].data
+    ye_in = cat_tab['ye'].data
+    me_in = cat_tab['me'].data
+
+    # Name is a unique name for each star and is a 1D array. 
+    name_in = cat_tab['name'].data
+    starlist_times = np.array([2001.0, 2002.1, 2003.0, 2004., 2005., 2006., 2007., 2008.])
+    starlist_names = np.array(['file1', 'file2', 'file3', 'file4', 'file5', 'file6', 'file7', 'file8'])
 
     # Generate the startable
-    startable = StarTable(x=x_tab, y=y_tab, m=m_tab, xe=xe_tab, ye=ye_tab,
-                        me=me_tab, name=name_tab, ref=1, time_lists=time_lists,
-                        name_lists=name_lists)
+    startable = StarTable(name=name_in, x=x_in, y=y_in, m=m_in, xe=xe_in, ye=ye_in, me=me_in,
+                              ref_list=1,
+                              list_times=starlist_times, list_names=starlist_names)
 
-    print('')  # Test line for inserting a debug breakpoint
-
+    # Now put in some assertions to make sure all our startable columns
+    # have the right dimensions.
+    assert len(startable) == N_stars
+    assert startable['x'].shape == (N_stars, N_lists)
+    assert startable['y'].shape == (N_stars, N_lists)
+    assert startable['m'].shape == (N_stars, N_lists)
+    assert startable['xe'].shape == (N_stars, N_lists)
+    assert startable['ye'].shape == (N_stars, N_lists)
+    assert startable['me'].shape == (N_stars, N_lists)
+    assert len(startable['name']) == N_stars
+    assert startable.meta['list_times'][0] == starlist_times[0]
+    
     return
 
 def test_StarTable_init2():
