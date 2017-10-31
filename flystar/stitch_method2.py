65da4368ed8b7b9a4008b96832a3f137e389ef64
Dongwon Kim <dongwon.kim@berkeley.edu>
2017-10-31 18:29:31 +0000

    stitch_method2.py edited online with Bitbucket

--- flystar/stitch_method2.py
+++ flystar/stitch_method2.py
@@ -229,9 +229,9 @@ def stitch(all_starlists, name_initial_ref, N_iter=5, corr_thresh=0.8,  outMaste
 		
 		#-------------Recalculate 'N_frames' for the master frame -> N_frames = the number of input starlists containing the star-----------
         # N_frames = the number of notnull columns at each row in the master frame divided by the number of columns in an input starlist, then minus one.
-        # The "minus one" at the end accounts for the very first columns, i.e. master columns, that contain the averaged values of all the input starlists.
+		# The "minus one" at the end accounts for the very first columns, i.e. master columns, that contain the averaged values of all the input starlists.
 
-        df_comb['N_frames'] = np.array(df_comb.notnull().sum(axis=1) / len(colnames) - 1)
+		df_comb['N_frames'] = np.array(df_comb.notnull().sum(axis=1) / len(colnames) - 1)
 
 		#-------------- Deal with the names of stars----------
 		Nanstar=df_comb['name'].isnull()
