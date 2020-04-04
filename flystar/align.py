ca6cc49b72312d7578f2356e95d8f4685c0179c5
Jessica Lu <jlu.astro@yahoo.com>
2020-04-03 17:42:38 -0700

    Oops

--- flystar/align.py
+++ flystar/align.py
@@ -401,7 +401,7 @@ class MosaicSelfRef(object):
             ### Initial match and transform: 1st order (if we haven't already).
             if trans == None:
                 # Only use "use_in_trans" reference stars, even for initial guessing.
-                keepers = np.where(ref_list[idx2]['use_in_trans'] == True)[0]
+                keepers = np.where(ref_list['use_in_trans'] == True)[0]
                 
                 trans = trans_initial_guess(ref_list[keepers], star_list_orig_trim, self.trans_args[0],
                                             mode=self.init_guess_mode,
