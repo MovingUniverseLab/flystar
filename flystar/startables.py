Fixing small weighting bug

flystar/startables.py
@@ -787,7 +787,7 @@ class StarTable(Table):
         elif N_good == 2:
             # Note nough epochs to fit a velocity.
             self['x0'][ss] = np.average(x, weights=1.0/xe**2)
-            self['y0'][ss] = np.average(y, weights=1.0/ye)
+            self['y0'][ss] = np.average(y, weights=1.0/ye**2)
             
             dx = np.diff(x)[0]
             dy = np.diff(y)[0]