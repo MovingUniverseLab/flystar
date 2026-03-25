f5102b1843078fdab3f8d74c3516530dad215dcc
“Lingfeng <lingfeng-wei@outlook.com>
2026-03-25 13:08:21 -0700

    Added seed for test function

--- flystar/tests/test_align.py
+++ flystar/tests/test_align.py
@@ -823,7 +823,7 @@ def test_MosaicToRef_mag_bug():
     """
     Bug found by Tuan Do on 2020-04-12.
     """
-    make_fake_starlists_poly1_vel()
+    make_fake_starlists_poly1_vel(seed=42)
 
     ref_list = starlists.StarList.read(f'{test_data_path}/random_vel_0.fits')
     lists = [ref_list]
@@ -1448,25 +1448,25 @@ def make_fake_starlists_poly1_par(seed=-1):
 if __name__ == '__main__':
     import pickle
     import matplotlib.pyplot as plt
-    with open(f'{test_data_path}/my_gaia.pkl', 'rb') as f:
-        my_gaia = pickle.load(f)
-    with open(f'{test_data_path}/list_of_starlists.pkl', 'rb') as f:
-        list_of_starlists = pickle.load(f)
-    ra_deg, dec_deg = 18.0, -30.0
-    my_gaia.remove_column('motion_model_used')
-    msc = align.MosaicToRef(my_gaia, list_of_starlists, iters=3,
-                        dr_tol=[0.2, 0.1, 0.08], dm_tol=[5,5,5],
-                        outlier_tol=[None, None, 3], mag_lim=[6, 20],
-                        trans_class=transforms.PolyTransform,
-                        trans_args=[{'order': 1}, {'order': 1}, {'order': 1}], 
-                        motion_models=['Empty','Fixed','Linear','Parallax'],
-                        fixed_params_dict = {'ra':ra_deg, 'dec':dec_deg, 'pa':0.0, 'obsLocation':'earth'},
-                        use_ref_new=True,
-                        update_ref_orig=False, 
-                        mag_trans=True,
-                        trans_weighting='both,std',
-                        init_guess_mode='name', verbose=3)
-    msc.fit()
-    for i in range(msc.ref_table['x'].shape[1]):
-        plt.scatter(msc.ref_table['x'][:, i], msc.ref_table['y'][:, i])
-    plt.show()
+    # with open(f'{test_data_path}/my_gaia.pkl', 'rb') as f:
+    #     my_gaia = pickle.load(f)
+    # with open(f'{test_data_path}/list_of_starlists.pkl', 'rb') as f:
+    #     list_of_starlists = pickle.load(f)
+    # ra_deg, dec_deg = 18.0, -30.0
+    # my_gaia.remove_column('motion_model_used')
+    # msc = align.MosaicToRef(my_gaia, list_of_starlists, iters=3,
+    #                     dr_tol=[0.2, 0.1, 0.08], dm_tol=[5,5,5],
+    #                     outlier_tol=[None, None, 3], mag_lim=[6, 20],
+    #                     trans_class=transforms.PolyTransform,
+    #                     trans_args=[{'order': 1}, {'order': 1}, {'order': 1}], 
+    #                     motion_models=['Empty','Fixed','Linear','Parallax'],
+    #                     fixed_params_dict = {'ra':ra_deg, 'dec':dec_deg, 'pa':0.0, 'obsLocation':'earth'},
+    #                     use_ref_new=True,
+    #                     update_ref_orig=False, 
+    #                     mag_trans=True,
+    #                     trans_weighting='both,std',
+    #                     init_guess_mode='name', verbose=3)
+    # msc.fit()
+    # for i in range(msc.ref_table['x'].shape[1]):
+    #     plt.scatter(msc.ref_table['x'][:, i], msc.ref_table['y'][:, i])
+    # plt.show()
